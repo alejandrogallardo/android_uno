3185176965ab83b0cb0f53c3874d61136a168a62
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-04-14 12:18:54 -0600

    Se crearon los botones M y F de IMCAPP

--- app/src/main/java/com/leksilab/firstapp/MenuActivity.kt
+++ app/src/main/java/com/leksilab/firstapp/MenuActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
+import com.leksilab.firstapp.imccalculator.ImcCalculatorActivity
 import com.leksilab.firstapp.uno.UnoAppActivity
 
 class MenuActivity : AppCompatActivity() {
@@ -11,7 +12,14 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         val btnSaludar = findViewById<AppCompatButton>(R.id.btnSaludar)
+        val btnImcApp = findViewById<AppCompatButton>(R.id.btnIMCApp)
         btnSaludar.setOnClickListener { navigateToSaludoApp() }
+        btnImcApp.setOnClickListener { navigateToImcApp() }
+    }
+
+    private fun navigateToImcApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
     }
 
     private fun navigateToSaludoApp() {
