3941f888c2ac7837ff68d876fd2a115573cfd5e7
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-05-15 16:01:31 -0600

    SuperHero app terminada

--- app/src/main/java/com/leksilab/firstapp/MenuActivity.kt
+++ app/src/main/java/com/leksilab/firstapp/MenuActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import com.leksilab.firstapp.imccalculator.ImcCalculatorActivity
+import com.leksilab.firstapp.superheroapp.SuperHeroListActivity
 import com.leksilab.firstapp.todoapp.TodoActivity
 import com.leksilab.firstapp.uno.UnoAppActivity
 
@@ -15,9 +16,11 @@ class MenuActivity : AppCompatActivity() {
         val btnSaludar = findViewById<AppCompatButton>(R.id.btnSaludar)
         val btnImcApp = findViewById<AppCompatButton>(R.id.btnIMCApp)
         val btnTODO = findViewById<AppCompatButton>(R.id.btnTODO)
+        val btnSuperHero = findViewById<AppCompatButton>(R.id.btnSuperHero)
         btnSaludar.setOnClickListener { navigateToSaludoApp() }
         btnImcApp.setOnClickListener { navigateToImcApp() }
         btnTODO.setOnClickListener { navigateToTODOApp() }
+        btnSuperHero.setOnClickListener { navigateToSuperHero() }
     }
 
     private fun navigateToTODOApp() {
@@ -32,6 +35,11 @@ class MenuActivity : AppCompatActivity() {
 
     private fun navigateToSaludoApp() {
         val intent = Intent(this, UnoAppActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSuperHero() {
+        val intent = Intent(this, SuperHeroListActivity::class.java)
         startActivity(intent)
     }
 }
